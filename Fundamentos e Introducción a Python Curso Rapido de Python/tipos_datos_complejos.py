3b75feb346cb821b45f663f10733cbe312895975
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-09-01 17:40:25 -0500

    tipos_datos_complejos

---
+++ Fundamentos e Introducción a Python Curso Rapido de Python/tipos_datos_complejos.py
@@ -0,0 +1,26 @@
+
+
+# Listas - Sintaxis []
+lista = ["tarea1","tarea2", "tarea3"]
+print(lista)
+print(type(lista))
+
+lista_numeros = [1,2,3,4,5]
+print(lista_numeros)
+
+lista_combinada = ["Tarea 1", 42, "Texto", [1,2,3,4,5]]
+print(lista_combinada)
+
+# Tuplas - Sintaxis () - Las tuplas son INMUTABLES
+tupla = ("tarea 1", "tarea 2", "tarea 3")
+print(tupla)
+print(type(tupla))
+
+
+# Diccionarios - Clave: Valor
+diccionario = {
+  "Tarea1": "Pendiente",
+  "Tarea2": "Taerminada"
+}
+print(type(diccionario))
+print(diccionario)
