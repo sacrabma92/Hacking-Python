49a2d24d22c9c12a5470438639a116f6688bcdc9
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-09-01 15:05:44 -0500

    Tipos de datos simples: Strings, Números y FStrings

---
+++ Fundamentos e Introducción a Python Curso Rapido de Python/tipos_datos_basicos.py
@@ -0,0 +1,24 @@
+
+print("Esto es un string que contiene comillas simples ''")
+print('Esto es un string que contiene comillas dobles ""')
+print("Saltos \nde linea en \nPython")
+
+#Forma de Escribir texto
+print("""
+Carlos
+Andres
+Ramirez
+""")
+
+# Aunque tengan varias se ejecuta en 1 linea
+print(("Joel"
+      "Santiago"
+      "Martinez"))
+
+# Tipo de dato numero
+print(10)
+print("10")
+print(type(10))
+
+#FString
+print(f"Cadena de texto combinada {1}, y otro numero {432}")
