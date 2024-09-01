87da29a2aa7fd610bd2aeb2ea529e465143a7b8e
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-09-01 14:52:48 -0500

    Create entrada_saluda_usuario.py

---
+++ Fundamentos e Introducción a Python Curso Rapido de Python/entrada_saluda_usuario.py
@@ -0,0 +1,9 @@
+
+print("Hola, bienvenido al Curso de Python")
+print("Hola, bienvenido al Curso de Python", "Clase 1")
+print("Hola, bienvenido al Curso de Python", "Clase 1", sep = " - ")
+print("Hola, bienvenido al Curso de Python", "Clase 1 ", sep = " - ", end="")
+print("Fundamentos e introduccioón a Python")
+
+
+print("Hola ", input("Introduce tu nombre: "), ", bienvenido al curso de Python")
