2f0b9dab5ac0addc04a3ccec252bd5f540baeccf
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-09-01 17:17:01 -0500

    Variables

---
+++ Fundamentos e Introducción a Python Curso Rapido de Python/variables.py
@@ -0,0 +1,10 @@
+
+
+nombre = ""
+nivel = ""
+
+nombre = input("Introduce tu nombre: ")
+print("Nombre del alumno:", nombre)
+nivel = input("Introduce el nivel de conocimiento (basico, medio, avanzado): ")
+print(f"Nivel de conocimiento sobre Python: {nivel}\n")
+
