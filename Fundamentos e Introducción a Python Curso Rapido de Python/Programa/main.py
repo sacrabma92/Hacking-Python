9cbd5482f2ca3dbfc25fe63247325e5d18c06c71
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-09-02 11:13:54 -0500

    Modulo y Paquetes

---
+++ Fundamentos e Introducción a Python Curso Rapido de Python/Programa/main.py
@@ -0,0 +1,11 @@
+import sys
+
+# import modulo1
+from modulo1 import *
+
+print(sys.path)
+
+test_func()
+
+
+print(vars)
---
+++ Fundamentos e Introducción a Python Curso Rapido de Python/Programa/modulo1.py
@@ -0,0 +1,6 @@
+def test_func():
+  print("Esto es una funcion del modulo 1")
+
+
+
+  variable = 32232
