e8cc85b6f4205caa767b4b7151967c9e93828c9b
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-09-01 21:20:29 -0500

    funciones

---
+++ Fundamentos e Introducción a Python Curso Rapido de Python/funciones.py
@@ -0,0 +1,20 @@
+
+
+def mi_funcion(arg1, arg2):
+  """
+  Esto es la documentacion de la funcion
+
+  Keyword arguments:
+  arg1 -- Primer argumento
+  arg2 -- Segundo argumento
+  """
+  print(arg1)
+  print(arg2)
+
+
+mi_funcion("hola", "mundo")
+
+def mi_funcion2():
+  print("Esto es una funcion sin argumentos")
+
+mi_funcion2()
