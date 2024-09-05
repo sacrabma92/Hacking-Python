e094d23d1d5ac5d3fd0ba0b07dd21f31828f87d5
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-09-05 08:03:43 -0500

    argparse

--- 1 Proyecto Google Hacking, IA y Automatización/1_1_HACKING_BUSCADORES/ninjadorks.py
+++ 1 Proyecto Google Hacking, IA y Automatización/1_1_HACKING_BUSCADORES/ninjadorks.py
@@ -1,8 +1,15 @@
-from dotenv import load_dotenv
-import os
+from dotenv import load_dotenv  # Libreria que ayuda a crear las variables de entorno
+import os   # Libreria que nos ayuda a interactuar con el OS
 from googlesearch import GoogleSearch
+import argparse    # Libreria que nos ayudara a pasar argumentos por linea de comandos
 
-def main():
+def main(query, configure_env, start_page, pages, lang):
+    # Comprobamos si existe el ficher .env
+    env_exists = os.path.exists(".env")
+
+    if not env_exists or configure_env:
+        pass
+
     #Cargamos las variables en el entorno
     load_dotenv()
 
@@ -21,4 +28,23 @@ def main():
     print(resultados)
 
 if __name__ == "__main__":
-    main
+    # Configuración de los argumentos del programa
+    parser = argparse.ArgumentParser(description="Esta herramienta permite realziar Hacking con buscadores de manera automatica")
+    parser.add_argument("-q","--query", type=str, 
+                        help="Especifica el dork que desea buscar.\nEjemplo: -q \"pass\" \"usuario\" filetype:sql")
+    parser.add_argument("-c", "--configure", action="store_true",
+                        help="Inicial el proceso de configuracion del archivo .env. \nUtiliza esta opcion sin otros argumentos para configurar las claves.")
+    parser.add_argument("--start-page", type=int, default=1,
+                        help="Define la pagina de inicio del buscador para obtener los resultados.")
+    parser.add_argument("--pages", type=int, default=1,
+                        help="Numero de paginas de resultados de busqueda.")
+    parser.add_argument("--lang", type=str, default="lang_es",
+                        help="Codigo de idioma para los resultados de busqueda. Por defecto es 'lang_es' español.")
+    # Almacenar los argumentos que le enviemos
+    args = parser.parse_args()
+
+    main(query=args.query,
+         configure_env=args.configure,
+         pages=args.pages,
+         start_page=args.start_page,
+         lang=args.lang)
