e8ad0695fb2bf875df3d1daeaf7d5d9575fb876f
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-09-07 10:05:15 -0500

    Avances

---
+++ 1 Proyecto Google Hacking, IA y Automatización/1_1_HACKING_BUSCADORES/result_parser.py
@@ -0,0 +1,34 @@
+import json
+
+
+class ResultParser():
+    """Forma en la cual se va a mostrar los datos ya sea en HTML o JSON"""
+    def __init__(self, resultados):
+        self.resultados = resultados
+
+    def exportar_html(self, archivo_salida):
+        """Metodo para crear la salida en formato HTML y crear el arhcivo .html"""
+        # Abrimos la plantilla en formato lectura
+        with open("html_template.html",'r', encoding='utf-8') as f:
+            plantilla = f.read()
+            
+        elementos_html = '' # Variable que almacenara las etiquetas html 
+        for indice, resultado in enumerate(self.resultados, start=1): # Creamos la estructura de la etiqueta HTML
+            elemento = f'<div class="resultado">' \
+                        f'<div class="indice">Resultado {indice}</div>' \
+                        f'<h5>{resultado["title"]}</h5>' \
+                        f'<p>{resultado["description"]}</p>' \
+                        f'<a href="{resultado["link"]}" target="_blank">{resultado["link"]}</a>' \
+                        f'</div>'
+            elementos_html += elemento
+        informe_html = plantilla.replace('{{ resultados }}', elementos_html) # Almacenamos en variable toda la estructura HTML creada
+        with open(archivo_salida, 'w', encoding='utf-8') as f: # Escribimos en la plantilla todo lo almacenado
+            f.write(informe_html)
+        print(f"Resultados exportados a HTML. Fichero creado: {archivo_salida}") # Mostramos mensaje cuando finalice
+    
+    def exportar_json(self, archivo_salida):
+        """Metodo para crear la salida en formato JSON y crear el arhcivo .json"""
+        with open(archivo_salida, 'w', encoding='utf-8') as f:
+            json.dump(self.resultados, f, ensure_ascii=False, indent=4)
+        print(f"Resultados exportados a JSON. Fichero creado: {archivo_salida}") # Mostramos mensaje cuando finalice
+
